b0666f39263eea44b19f485abdb7ad3fcc360b86
Julien Karst 📱 <contact@julienkarst.com>
2018-02-02 15:15:45 -0800

    [WIP]

---
+++ Calculatrice/CalculatricePage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Calculatrice
+{
+    public partial class CalculatricePage : ContentPage
+    {
+        public CalculatricePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
