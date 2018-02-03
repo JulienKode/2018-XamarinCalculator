c65f96ae1bac52bedc878ceb00a6ccbacc25cc50
Julien Karst 📱 <contact@julienkarst.com>
2018-02-02 17:00:45 -0800

    [WIP]

--- Calculatrice/CalculatricePage.xaml.cs
+++ Calculatrice/CalculatricePage.xaml.cs
@@ -1,9 +1,14 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
 
 namespace Calculatrice
 {
     public partial class CalculatricePage : ContentPage
     {
+        int state = 0;
+        int lastNb = 0;
+
         public CalculatricePage()
         {
             InitializeComponent();
@@ -11,8 +16,34 @@ namespace Calculatrice
 
         void Handle_Clicked(object sender, System.EventArgs e)
         {
-            
+            Button button = (Button)sender;
+            string pressed = button.Text;
+
+            Debug.WriteLine(pressed);
+
+            if (Int32.TryParse(pressed, out lastNb))
+                Debug.WriteLine(lastNb);
+            else
+                Debug.WriteLine("String could not be parsed.");
+
+            switch (pressed) 
+            {
+                case "+":
+                    state += lastNb;
+                    break;
+                case "-":
+                    state -= lastNb;
+                    break;
+                case "/":
+                    state = state / lastNb;
+                    break;
+                case "x":
+                    state = state * lastNb;
+                    break;
+            }
+
             //throw new NotImplementedException();
         }
+
     }
 }
