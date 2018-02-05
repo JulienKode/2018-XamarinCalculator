f7f555868bf61395bb8f78755bc981dcc3c842ea
Julien Karst 📱 <contact@julienkarst.com>
2018-02-04 21:31:22 -0800

    [Calculator] Multinumber in progress

---
+++ Calculatrice/Calculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Calculatrice
+{
+    public class Calculator
+    {
+        int state = 0;
+
+        string op = null;
+
+        int second = 0;
+
+        public Calculator()
+        {
+        }
+
+        void MakeCalcul(string op)
+        {
+            Debug.WriteLine(op);
+            switch (op)
+            {
+                case "+":
+                    state = state + second;
+                    break;
+                case "-":
+                    state = state - second;
+                    break;
+                case "/":
+                    state = state / second;
+                    break;
+                case "x":
+                    state = state * second;
+                    break;
+            }
+            Debug.WriteLine(op);
+            Debug.WriteLine(state.ToString());
+            op = null;
+
+        }
+
+
+        void ManageCalcul(string pressed)
+        {
+            int nb;
+            if (Int32.TryParse(pressed, out nb))
+            {
+                if (firstType == true) {
+                    state = nb;
+                    firstType = false;
+                }
+                else
+                    second = nb;
+                Debug.WriteLine("State");
+                Debug.WriteLine(state);
+                Debug.WriteLine("Second");
+                Debug.WriteLine(second);
+            }
+            else if (pressed.Equals("="))
+            {
+                Debug.WriteLine("Equals");
+                MakeCalcul(op);
+            }
+            else
+            {
+                Debug.WriteLine("Operator");
+                Debug.WriteLine(pressed);
+                op = pressed;
+            }
+        }
+
+        void Reset() {
+            
+        }
+    }
+}
--- Calculatrice/CalculatricePage.xaml.cs
+++ Calculatrice/CalculatricePage.xaml.cs
@@ -6,54 +6,9 @@ namespace Calculatrice
 {
     public partial class CalculatricePage : ContentPage
     {
-        int state = 0;
-        int first = 0;
-        string op = null;
-        int second = 0;
-
         public CalculatricePage()
         {
             InitializeComponent();
-        }
-
-        void ManageCalcul(string pressed)
-        {
-            int nb;
-            if (Int32.TryParse(pressed, out nb))
-            {
-                Debug.WriteLine(nb);
-                if (op == null)
-                    first = nb;
-                else
-                    second = nb;
-            } else if (string.ReferenceEquals(pressed, "="))
-            {
-                MakeCalcul();
-            } else {
-                op = pressed;
-            }
-        }
-
-        void MakeCalcul() {
-            switch (op)
-            {
-                case "+":
-                    state = first + second;
-                    break;
-                case "-":
-                    state = first - second;
-                    break;
-                case "/":
-                    state = first / second;
-                    break;
-                case "x":
-                    state = first * second;
-                    break;
-            }
-            Debug.WriteLine(state.ToString());
-            result.Text = state.ToString();
-            op = null;
-
         }
 
         void Handle_Clicked(object sender, System.EventArgs e)
@@ -61,8 +16,8 @@ namespace Calculatrice
             Button button = (Button)sender;
             string pressed = button.Text;
 
-            ManageCalcul(pressed);
-
+            string nb = Calculator.ManageCalcul(pressed);
+            result.Text = nb;
         }
 
     }
